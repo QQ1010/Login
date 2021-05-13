5c1e44b828f8b9c0a8faa00785dffa422976d202
QQ <asdfg335522@gmail.com>
2021-05-14 00:03:42 +0800

    2021.05.13

---
+++ src/components/Signin.vue
@@ -0,0 +1,72 @@
+<template>
+ <v-form>
+   <v-container>
+     <v-row>
+       <v-col
+       cols="12"
+       sm="6"
+       md="3"
+       >
+       <v-text-field
+       v-model="username"
+       label="Username"
+       :rules="[() => !!username || 'This field is required.']"
+       :error-messages="errorMessages"
+       clearable
+       >
+       </v-text-field>
+       </v-col>
+     </v-row>
+     <v-row>
+       <v-col
+        cols="12"
+        sm="6"
+        md="3"
+       >
+        <v-text-field
+        v-model="password"
+        label="Password"
+        :rules="[() => !!password || 'This field is required.']"
+        :error-messages="errorMessages"
+        clearable
+        >
+        </v-text-field>
+      </v-col>
+     </v-row>
+   <v-card-actions>
+   <v-row>
+     <v-col
+     cols="12"
+     sm="6"
+     >
+     <v-btn
+     depressed
+     @click="check"
+     >
+     Submit
+     </v-btn>
+     </v-col>
+   </v-row>
+   </v-card-actions>
+   </v-container>
+ </v-form>
+</template>
+
+<script>
+export default {
+  name: 'Signin',
+  data () {
+    return {
+      username: '',
+      password: '',
+      errorMessages: ''
+    }
+  },
+  methods: {
+    check () {
+      if (this.username === 'admin' && this.password === 'csieweek') alert('Lodin success!')
+      else alert('Wrong Answer')
+    }
+  }
+}
+</script>
